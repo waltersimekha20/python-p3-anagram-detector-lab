31229877da87f2b592e0de667f59ad4d0294cbf2
waltersimekha <waltersimekha@gmail.com>
2024-03-12 09:08:39 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+# Save this code in a file named 'anagram.py'
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted(w.lower()) == sorted(self.word.lower()) and w.lower() != self.word.lower()]
